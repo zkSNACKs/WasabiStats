4448a17873e5c5cd0511689f05c73ee8c7b499ac
BaliGabi <baligabor81@gmail.com>
2022-04-01 10:51:54 +0200

    shortcut icon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Wasabi Wallet - Statistics</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/logo.svg')}}">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
