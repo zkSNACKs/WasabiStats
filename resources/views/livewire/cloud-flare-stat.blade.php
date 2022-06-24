cff3fd1ef4bffcbae8f36768872a0f1e86d52a73
Lévai Norbert <norbert.levai@creativeagent.hu>
2022-06-24 17:23:36 +0200

    remove css mapping

--- resources/views/livewire/cloud-flare-stat.blade.php
+++ resources/views/livewire/cloud-flare-stat.blade.php
@@ -3,7 +3,7 @@
         @if($cloudFlareStatus['status']['indicator'] == 'none') green
         @elseif($cloudFlareStatus['status']['indicator'] == 'minor') yellow
         @elseif($cloudFlareStatus['status']['indicator'] == 'major') orange
-        @else red @endif">
-    </span> {{$cloudFlareStatus['status']['indicator']}}</p>
+        @else red @endif" data-bs-toggle="tooltip" data-bs-placement="top" title="{{$cloudFlareStatus['status']['indicator']}}">
+    </span></p>
     <!--<p class="small-text">Checked: {{(date('Y-m-d H:m:s T', strtotime($cloudFlareStatus['page']['updated_at'])))}}</p>-->
 </div>
