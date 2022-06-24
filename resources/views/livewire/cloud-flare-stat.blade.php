4673fe7c11cab5ea9b9a0bbc205e565246e72b3e
Lévai Norbert <norbert.levai@creativeagent.hu>
2022-06-24 18:00:00 +0200

    cloudflare stat style

--- resources/views/livewire/cloud-flare-stat.blade.php
+++ resources/views/livewire/cloud-flare-stat.blade.php
@@ -1,9 +1,9 @@
 <div class="feature-box cloudflare text-light">
-    <p>CloudFlare Status: <span class="status-dot
+    <p><span class="status-dot
         @if($cloudFlareStatus['status']['indicator'] == 'none') green
         @elseif($cloudFlareStatus['status']['indicator'] == 'minor') yellow
         @elseif($cloudFlareStatus['status']['indicator'] == 'major') orange
         @else red @endif" data-bs-toggle="tooltip" data-bs-placement="top" title="{{$cloudFlareStatus['status']['indicator']}}">
-    </span></p>
+    </span> CloudFlare Status</p>
     <!--<p class="small-text">Checked: {{(date('Y-m-d H:m:s T', strtotime($cloudFlareStatus['page']['updated_at'])))}}</p>-->
 </div>
