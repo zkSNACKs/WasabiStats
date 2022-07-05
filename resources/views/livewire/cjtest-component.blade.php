98994cfd143bc3a974ae1205ff2a5f105a158850
Lévai Norbert <norbert.levai@creativeagent.hu>
2022-07-05 15:43:57 +0200

    Change colors

--- resources/views/livewire/cjtest-component.blade.php
+++ resources/views/livewire/cjtest-component.blade.php
@@ -5,12 +5,12 @@
             @foreach ($cjTest as $key => $item)
                 <div class="col-md-3 mb-3">
                     <div class="feature-box">
-                        <p class="mb-0"><strong>roundId:</strong> <span class="text-green">{{$item['roundId']}}</span></p>
+                        <p class="mb-0"><strong>roundId:</strong> <span class="text-blue">{{$item['roundId']}}</span></p>
                         <p class="mb-0"><strong>isBlameRound:</strong> <span class="text-pink">{{$item['isBlameRound']}}</span></p>
                         <p class="mb-0"><strong>inputCount:</strong> <span class="text-orange">{{$item['inputCount']}}</p>
                         <p class="mb-0"><strong>maxSuggestedAmount:</strong> <span class="text-orange">{{$item['maxSuggestedAmount']}}</span></p>
-                        <p class="mb-0"><strong>inputRegistrationRemaining:</strong> <span class="text-green">{{$item['inputRegistrationRemaining']}}</span></p>
-                        <p><strong>phase:</strong> <span class="text-green">{{$item['phase']}}</span></p>
+                        <p class="mb-0"><strong>inputRegistrationRemaining:</strong> <span class="text-blue">{{$item['inputRegistrationRemaining']}}</span></p>
+                        <p><strong>phase:</strong> <span class="text-blue">{{$item['phase']}}</span></p>
                     </div>
                 </div>
             @endforeach
