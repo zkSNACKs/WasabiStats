60f3e32725cdf36abac758f337171b1ad4cb0f59
BaliGabi <baligabor81@gmail.com>
2022-07-07 12:20:17 +0200

    humanmonitor fix

--- resources/views/livewire/cjtest-component.blade.php
+++ resources/views/livewire/cjtest-component.blade.php
@@ -6,7 +6,7 @@
                 <div class="col-md-3 mb-3">
                     <div class="feature-box">
                         <p class="mb-0"><strong>roundId:</strong> {{$item['roundId']}}</p>
-                        <p class="mb-0"><strong>isBlameRound:</strong> {{$item['isBlameRound']}}</p>
+                        <p class="mb-0"><strong>isBlameRound:</strong> {{$item['isBlameRound']? 'true':'false'}}</p>
                         <p class="mb-0"><strong>inputCount:</strong> {{$item['inputCount']}}</p>
                         <p class="mb-0"><strong>maxSuggestedAmount:</strong> {{$item['maxSuggestedAmount']}}</p>
                         <p class="mb-0"><strong>inputRegistrationRemaining:</strong> {{$item['inputRegistrationRemaining']}}</p>
