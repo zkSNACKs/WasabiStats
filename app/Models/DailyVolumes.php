c940be71104e3f69212a08d2292326142a927645
Lévai Norbert <chupa92@gmail.com>
2023-02-09 14:55:49 +0100

    Fix class typo

--- app/Models/DailyVolumes.php
+++ app/Models/DailyVolumes.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DailyVolumes extends Model
+class DailyVolume extends Model
 {
     use HasFactory;
 
