2cd2b2a3fe52e24c02c7662564bd6d11f68d3af2
Lévai Norbert <chupa92@gmail.com>
2024-01-11 10:12:36 +0100

    Add min input amount to testnet CJ rounds

--- app/Http/Livewire/CjtestComponent.php
+++ app/Http/Livewire/CjtestComponent.php
@@ -13,6 +13,41 @@ class CjtestComponent extends Component
     {
         try {
             $cjTest = Http::get('https://wasabiwallet.co/WabiSabi/human-monitor')->json()['roundStates'];
+            $statusUrl = 'https://wasabiwallet.co/WabiSabi/status';
+
+            $headers = [
+                'Accept: application/json',
+                'Content-Type: application/json-patch+json',
+            ];
+            foreach ($cjTest as $key => $value) {
+                $data = [
+                    'roundCheckpoints' => [
+                        [
+                            'roundId' => $value['roundId'],
+                            'stateId' => 0,
+                        ],
+                    ],
+                ];
+
+                $ch = curl_init($statusUrl);
+
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+                $response = curl_exec($ch);
+
+                if (curl_errno($ch)) {
+                    echo 'Curl error: ' . curl_error($ch);
+                }
+
+                curl_close($ch);
+                if(isset(json_decode($response, true)['roundStates'][0]['coinjoinState']['events'][0]['roundParameters']['allowedInputAmounts']))
+                {
+                    $cjTest[$key]['allowedInputAmounts'] = json_decode($response, true)['roundStates'][0]['coinjoinState']['events'][0]['roundParameters']['allowedInputAmounts'];
+                }
+            }
             $nodata = null;
         } catch (\Exception $th) {
             $nodata = 'No data from server!';
