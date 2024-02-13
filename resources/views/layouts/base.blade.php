fdfadcd747e2f12a5de01bf1ff596152b812209c
Lévai Norbert <chupa92@gmail.com>
2024-02-13 19:21:49 +0100

    Add csrf_token

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf_token" value="{{ csrf_token() }}"/>
 	<title>Wasabi Wallet - Statistics</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/img/logo.svg')}}">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
